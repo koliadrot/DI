80a12d06b8399f602c4a870ea5f33fbf89789adf
Nikolai <koliadrot@mail.ru>
2022-12-16 03:10:12 +0200

    Реализация инициа-ии Editor ExecutironOrder скриптов DI.

---
+++ Assets/DI/Editor/ExecutionOrderCheckerEditor.cs
@@ -0,0 +1,50 @@
+﻿namespace UnityDev.DI
+{
+    using System;
+    using System.Linq;
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Проверяет перед запуском у ключевых скриптов
+    /// порядок инициа-ции перед стартом.
+    /// </summary>
+    public class ExecutionOrderCheckerEditor
+    {
+        /// <summary>
+        /// Автовалидация порядка вызовов
+        /// </summary>
+        [MenuItem(nameof(UnityDev) + "/" + nameof(DI) + "/" + nameof(Validate))]
+        public static void Validate()
+        {
+            int startOrder = -10000;
+
+            SetExecutionOrder(typeof(DIContanier), ref startOrder);
+        }
+
+        /// <summary>
+        /// Автовалидация порядка вызовов с ивентом
+        /// </summary>
+        /// <param name="action"></param>
+        public static void Validate(Action action)
+        {
+            Validate();
+            action?.Invoke();
+        }
+
+        private static void SetExecutionOrder(Type type, ref int order)
+        {
+            MonoScript objectExecution = MonoImporter.GetAllRuntimeMonoScripts().Where(x => x.name == type.Name).FirstOrDefault();
+
+            if (objectExecution != null)
+            {
+                MonoImporter.SetExecutionOrder(objectExecution, order);
+                order++;
+            }
+            else
+            {
+                Debug.LogError($"Не найден MonoScript {type.Name} для ExecutionOrder");
+            }
+        }
+    }
+}
---
+++ Assets/DI/Editor/GetVersionPluginEditor.cs
@@ -0,0 +1,16 @@
+﻿namespace UnityDev.DI
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    /// <summary>
+    /// Editor скрипт для определения текущей версии плагина.
+    /// </summary>
+    public class GetVersionPluginEditor
+    {
+        private const string CURRENT_VERSION = "1.0.1";
+
+        [MenuItem("UnityDev/DI/Version - " + CURRENT_VERSION)]
+        public static void GetVersionInfo() => Debug.Log("DI версия - " + CURRENT_VERSION);
+    }
+}
